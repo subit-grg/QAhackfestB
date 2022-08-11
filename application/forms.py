18bdc840306842966039650716bedf4b57087b47
Subit <subit.grg@gmail.com>
2022-08-11 11:56:09 +0000

    updates

---
+++ application/forms.py
@@ -0,0 +1,59 @@
+from flask_wtf import FlaskForm
+
+from wtforms import StringField, SubmitField, SelectField, TextAreaField, DateField
+
+from wtforms.validators import DataRequired, ValidationError
+
+from datetime import date
+
+
+
+class checkDateInFuture():
+
+    def __init__(self, message):
+
+        self.message = message
+
+   
+
+    def __call__(self, form, field):
+
+        if field.data is None or field.data < date.today():
+
+            raise ValidationError(self.message)
+
+
+
+
+class DriverForm(FlaskForm):
+
+    forename = StringField('Enter Forename', validators=[DataRequired()])
+
+    surname = StringField('Enter Surname', validators=[DataRequired()])
+
+    location = StringField('Enter your depot', validators=[DataRequired()])
+
+    submit = SubmitField('Enter')
+
+
+
+
+class DeliveryForm(FlaskForm):
+
+    destination = StringField('Destination Address', validators=[DataRequired()])
+
+    delivery_date = DateField('Delivery Date', validators=[checkDateInFuture("Please choose a date in the future")])
+
+    delivered_by = SelectField('Delivered By', choices=[])
+
+    submit = SubmitField('Enter')
+
+   
+
+class OrderForm(FlaskForm):
+
+    order_name = StringField('Order Name', validators=[DataRequired()])
+
+    ordered_items = TextAreaField('Ordered Items', validators=[DataRequired()])
+
+    order_date = DateField('Order Date')
---
+++ application/routes.py
@@ -0,0 +1,9 @@
+from application import app, db
+from application.models import *
+from datetime import date, timedelta
+from flask import request, redirect, url_for, render_template
+from application.forms import *
+
+
+
+
