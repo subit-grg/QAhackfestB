cf81972f8ea4d82c4a77069b5053805068b8e2af
Subit <subit.grg@gmail.com>
2022-08-11 13:22:48 +0000

    updates

--- application/__init__.py
+++ application/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookreview-db.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///logistics-db.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 app.config['SECRET_KEY'] = 'redbullwings2'
